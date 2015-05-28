b93e1878570e4e137306faf72d2a6f6a93a17c6b
WebEpic <randy@randyjmitchell.com>
2015-05-28 07:14:16 -0400

    added basic_auth, methods(partial) for api calls and passing data to database

--- lib/codecreep.rb
+++ lib/codecreep.rb
@@ -5,15 +5,50 @@ require 'pry'
 require 'codecreep/init_db'
 require 'codecreep/github'
 require "codecreep/version"
+require 'codecreep/users'
 
 
 module Codecreep
+
+  base_uri 'https://api.github.com'
+  basic_auth ENV['GH_USER'], ENV['GH_PASS']
+
   class App
     def initialize
       @github = Github.new
     end
   end
+
+  class CoderCreep
+
+    def following
+      self.class.get(users/["thezerobit", "eudoxia0", "thepeopleseason", 
+        "adriennefriend", "agumonkey", "WebEpic", "masondesu", "chefyangzhao", 
+        "longlene", "hanziwang"].to_json/following)
+    end
+
+    def followers
+      self.class.get(users/["thezerobit", "eudoxia0", "thepeopleseason", 
+        "adriennefriend", "agumonkey", "WebEpic", "masondesu", "chefyangzhao", 
+        "longlene", "hanziwang"].to_json/followers)
+    end
+
+    def user
+      params = {
+        name: name,
+        follower_count: follower_count,
+        friend_count: friend_count,
+        repo_count: repo_count,
+        company: company,
+        homepage: homepage,
+      }
+    end
+
+  end
+
 end
 
 # app = Codecreep::App.new
 # binding.pry
+
+
